c4b40b53ff8474dfdf423182632443a72c2e0c17
ranjak <ranjak.hector@gmail.com>
2018-04-02 13:42:05 +0200

    Implement directory listing.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::io;
 use std::error::Error;
 
 fn main() {
+	// Accept a directory argument from command line, or use the working directory.
 	let dir_to_list = if env::args().count() == 2 {
 		get_path_from_arg(&env::args().nth(1).unwrap())
 	}
@@ -18,6 +19,11 @@ fn main() {
 	});
 	
 	println!("Directory to list: {}", dir_to_list.display());
+	
+	list_directory_entries(dir_to_list.as_path()).unwrap_or_else(|e| {
+		eprintln!("Error listing directory entries: {}", e.description());
+		std::process::exit(1);
+	});
 }
 
 // Make sure the argument is a valid path
@@ -31,4 +37,22 @@ fn get_path_from_arg(arg: &String) -> std::io::Result<path::PathBuf> {
 	else {
 		Ok(target_dir)
 	}
+}
+
+fn list_directory_entries(dir: &path::Path) -> std::io::Result<()> {
+	for entry in dir.read_dir()? {
+		let entry = entry?;
+		let entry_type = match entry.file_type()? {
+			t if t.is_dir() => "Directory",
+			t if t.is_file() => "File",
+			t if t.is_symlink() => "Symlink",
+			_ => "Unknown"
+		};
+		let entry_name = entry.file_name().into_string().unwrap_or_else(|_| {
+			String::from("<Error: name contains invalid unicode data>")
+		});
+		
+		println!("[{}] {}", entry_type, entry_name);
+	}
+	Ok(())
 }
